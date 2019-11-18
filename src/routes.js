55d0acaa343ed8d4c608d82a24755af3c99aa926
kallelrm <kallelr@outlook.com>
2019-11-18 17:39:15 -0300

    Configurando o debugger

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) =>{
-  return res.json( { ok: true } );
+  return res.json( { mensagem: "kallel" } );
 });
 
 export default routes;

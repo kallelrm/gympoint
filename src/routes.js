b049b6c3bacf72c69875d89de464712b96f6e16e
kallelrm <kallelr@outlook.com>
2019-11-20 15:54:27 -0300

    Criando o loader dos models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import Student from './app/models/Students';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ mensagem: 'kallel' }));
+routes.get('/', async (req, res) => {
+  const student = await Student.create({
+    name: 'Kallel Roman',
+    email: 'kallelr@outlook.com',
+    age: '19',
+    weight: 68.4,
+    height: 1.72,
+  });
+
+  return res.json(student);
+});
 
 export default routes;

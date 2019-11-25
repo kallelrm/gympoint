1b6ba1e162e171977492e6c1a5aeaadf729f1151
kallelrm <kallelr@outlook.com>
2019-11-25 13:49:55 -0300

    Instalando jsonwebtoken | configurando criação do token jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8cb39b1eb50538024cced6ebec408e19',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);

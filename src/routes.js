c26beedee56a3b19e1ce84f789f3a0dd4905a3f6
kallelrm <kallelr@outlook.com>
2019-11-26 15:56:00 -0300

    Criando o middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.get('/students', StudentController.index);
 routes.post('/students', StudentController.store);

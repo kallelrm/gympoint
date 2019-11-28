88146acedfb05bfaba0ff03df76daabca580aa19
kallelrm <kallelr@outlook.com>
2019-11-28 10:55:15 -0300

    Criando a validação no StudentController

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,3 +1,5 @@
+import * as Yup from 'yup';
+
 import Students from '../models/Students';
 
 class StudentController {
@@ -8,6 +10,18 @@ class StudentController {
   }
 
   async store(req, res) {
+    const schema =  Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      age: Yup.number().required(),
+      height: Yup.number().required(),
+      weight: Yup.number().required()
+    });
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ Error: "Validation failure" });
+    }
+
     const studentExists = await Students.findOne({
       where: { email: req.body.email },
     });
@@ -31,6 +45,22 @@ class StudentController {
 
   async update(req, res) {
     const { email, name, age, height, weight } = req.body;
+
+    if(typeof(name) !== 'string'){
+      return res.status(400).json({ Error: "Validation Failure" });
+    }
+
+    const schema =  Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email().required(),
+      age: Yup.number(),
+      height: Yup.number(),
+      weight: Yup.number()
+    });
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ Error: "Validation failure" });
+    }
 
     if (!email) {
       return res.status(400).json({ error: 'Email not provided' });

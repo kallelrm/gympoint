b07973f5526f0a481a8ac34bdb6eaa9fb533bffb
kallelrm <kallelr@outlook.com>
2019-12-30 18:48:20 -0300

    Fazendo cálculo de preço total e mostrando no "front"

--- src/app/models/Plans.js
+++ src/app/models/Plans.js
@@ -7,6 +7,12 @@ class Plans extends Model {
         title: Sequelize.STRING,
         duration: Sequelize.INTEGER,
         price: Sequelize.FLOAT,
+        total_price: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return this.getDataValue('price') * this.getDataValue('duration');
+          },
+        },
       },
       {
         sequelize,
